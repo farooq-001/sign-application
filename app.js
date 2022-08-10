2dd4b446ed865091cb7c22e25b760f5f32222ff3
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2022-08-10 11:02:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express=require("express");
 var bodyParser=require("body-parser");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv:m001-student:m001-mongodb-basics@sandbox.zxo60va.mongodb.net/myFirstDatabase'
-);
+mongoose.connect('mongodb+srv:m001-student:m001-mongodb-basics@sandbox.zxo60va.mongodb.net/myFirstDatabase');
+
 var db=mongoose.connection;
 db.on('error', console.log.bind(console, "connection error"));
 db.once('open', function(callback){
